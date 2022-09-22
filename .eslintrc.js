fd460d5166e87588c3f98cead23180a7805b73d9
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-09-22 16:42:13 +0500

    update-git-ignore

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};

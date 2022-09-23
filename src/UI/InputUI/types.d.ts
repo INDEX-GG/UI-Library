09e1cace064bdcfe474e073044b0b92dc2bcd533
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-09-23 09:57:35 +0500

    change readme.md, pachkage.json, tsconfig.json and other changes

--- src/UI/InputUI/types.d.ts
+++ src/UI/InputUI/types.d.ts
@@ -1,5 +1,10 @@
 import { InputHTMLAttributes } from "react";
+import { StyledComponent } from "styled-components";
+import { ThemeType } from "@/types/types";
 
 export interface IInputUIProps {
   inputProps?: InputHTMLAttributes<HTMLInputElement>;
+  styledComponents?: {
+    Input?: StyledComponent<"input", ThemeType>;
+  };
 }

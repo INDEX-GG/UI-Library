965ce8b74748c041634444844cc9f025d25031e8
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-09-26 11:48:18 +0500

    update input ui

--- src/UI/InputUI/types.ts
+++ src/UI/InputUI/types.ts
@@ -7,9 +7,19 @@ export const InputUIStyledComponent = useInputUIStyles();
 //export types
 export type IconPositionType = "right" | "left" | "auto";
 
+type excludesInputProps = "type" | "name" | "inputMode";
+
+// inputProps
+type InputPropsType = InputHTMLAttributes<HTMLInputElement>;
+// required Input props
+type RequiredInputProps = Required<Pick<InputPropsType, excludesInputProps>>;
+// not required Input props
+type NotRequiredInputProps = Omit<InputPropsType, excludesInputProps>;
+
 // types
 type inputStyledComponentsType = Partial<typeof InputUIStyledComponent>;
-export type InputPropsType = InputHTMLAttributes<HTMLInputElement>;
+export type CustomInputPropsType = RequiredInputProps & NotRequiredInputProps;
+
 type errorType = {
   isError: boolean;
   errorMessage: string;
@@ -27,14 +37,18 @@ type iconObjType = {
 export interface IInputUIProps {
   iconObj?: iconObjType;
   error?: errorType;
-  inputProps?: InputPropsType;
+  inputProps?: CustomInputPropsType;
   styledComponents?: inputStyledComponentsType;
 }
 
 // default constants
 export const InputUIDefaultStyledComponents: inputStyledComponentsType =
   InputUIStyledComponent;
-export const InputUIDefaultInputProps: InputPropsType = {};
+export const InputUIDefaultInputProps: CustomInputPropsType = {
+  type: "text",
+  inputMode: "text",
+  name: "",
+};
 export const InputUIDefaultError: errorType = {
   isError: false,
   errorMessage: "",

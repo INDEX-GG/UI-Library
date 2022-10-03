7affb20a08547f98ad9e1db9858999731a2973b5
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-10-03 15:18:28 +0500

    forward-update: delete useless import

--- src/stories/components/TextAreaUI.stories.tsx
+++ src/stories/components/TextAreaUI.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import "css/global.css";
 import { TextAreaUI } from "UI/TextAreaUI/TextAreaUI";
